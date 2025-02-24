5d116fa95353cf202428fef69c0b8168fa388ae0
javad <j.2528840@gmail.com>
2025-02-24 17:34:47 +0330

    api

---
+++ network_api.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+def my_function(name: str) -> str:
+    return f"Hello, {name}!"
+
+
+@app.get("/execute/{name}")
+async def execute_function(name: str):
+    result = my_function(name)
+    return {"result": result}
+
+# uvicorn main:app --reload
